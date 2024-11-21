7084b7d92dfd55c0ad3c88156d52e176154f4f37
rajat693 <rajatchaudhary.rc693@gmail.com>
2024-11-21 14:02:21 +0530

    fix: docs examples

--- components/docs/examples/heading/index.js
+++ components/docs/examples/heading/index.js
@@ -6,7 +6,7 @@ export const examples = [
   {
     name: "Heading Sizes",
     Code: (
-      function App() { const sizes = [ 'xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', ]; return (<Center>{sizes.map((size) =>(<Heading size={size}>{size}</Heading>))}</Center>); }
+      function App() { const sizes = [ 'xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', ]; return (<Center>{sizes.map((size, index) =>(<Heading size={size} key={index}>{size}</Heading>))}</Center>); }
     )
   },
 ]
