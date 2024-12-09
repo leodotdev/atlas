2bc44cccd17d515859310e56861ee2656691e590
rajat693 <rajatchaudhary.rc693@gmail.com>
2024-12-09 15:14:01 +0530

    fix: home page ui in native

--- app/index.tsx
+++ app/index.tsx
@@ -24,7 +24,7 @@ const ComponentCard = ({ component, onPress }: any) => {
       className={`flex-1 rounded-xl bg-background-0 w-full h-full sm:gap-2 gap-1 flex flex-col lg:p-4 ${colorMode === "light" ? "lg:shadow-[0px_0px_4.374px_0px_rgba(38,38,38,0.10)] data-[hover=true]:lg:border data-[hover=true]:border-outline-100" : "lg:shadow-soft-1 lg:border border-outline-50 data-[hover=true]:border-outline-200"}`}
       onPress={onPress}
     >
-      <Box className="flex-1 rounded-lg bg-background-50 px-3 lg:px-6 py-[14px] lg:py-7 aspect-[17/12]">
+      <Box className="rounded-lg bg-background-50 px-3 lg:px-6 py-[14px] lg:py-7 aspect-[17/12]">
         <ExpoImage
           source={{
             uri: colorMode === "light" ? component.url : component.darkUrl,
@@ -106,12 +106,12 @@ export default function HomeScreen() {
           <Header />
         </Box>
         <HStack
-          className="flex-wrap justify-center native:gap-6 gap-x-3 gap-y-4 md:gap-x-4 lg:gap-x-7 lg:gap-y-8 py-6 px-5 md:px-8 md:pt-9 xl:pt-[90px] lg:pt-[70px] lg:px-[70px] xl:px-[100px] max-w-[1730px] mx-auto"
+          className="flex-wrap justify-center gap-x-3 gap-y-4 md:gap-x-4 lg:gap-x-7 lg:gap-y-8 py-6 px-5 md:px-8 md:pt-9 xl:pt-[90px] lg:pt-[70px] lg:px-[70px] xl:px-[100px] max-w-[1730px] mx-auto"
         >
           {componentsList.map((component, index) => (
             <Box
               key={index}
-              className="w-[150px] h-[142px] md:w-[224px] md:h-[194px] lg:w-[274px] lg:h-[244px] xl:w-[390px] xl:h-[328px]"
+              className="w-[160px] h-[145px] md:w-[224px] md:h-[194px] lg:w-[274px] lg:h-[244px] xl:w-[390px] xl:h-[328px]"
             >
               <ComponentCard
                 component={component}
